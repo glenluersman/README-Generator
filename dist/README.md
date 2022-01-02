23733dba5306c070fecb3aeae72004ac3f884452
glenluersman <glen.luersman@gmail.com>
2022-01-02 09:35:24 -0500

    generateReadme.js created

---
+++ dist/README.md
@@ -0,0 +1,32 @@
+
+<h1 align="center">Readme Genenerator</h1>
+  
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+## Description
+- generates a readme.md markdown file
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+- none
+## Usage
+- generating a readme.md file
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+<br />
+This application is covered by the MIT license. 
+## Contribution
+- glenluersman
+## Tests
+- no
+## Questions
+- Find me on GitHub: [glenluersman](https://github.com/glenluersman)<br />
+<br />
+- Email me with any questions: glen.luersman@gmial.com<br /><br />
+_This README was generated with ❤️ by [README-generator](https://github.com/glenluersman/README-Generator)
+    
--- index.js
+++ index.js
@@ -1,15 +1,83 @@
+const fs = require("fs");
+const util = require("util");
+const generateReadme = require("./utils/generateReadme")
 const inquirer = require("inquirer");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Include packages needed for this application
-inquirer
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a brief description: '
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe the installation of your project: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is your project used for?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Were there any contributors? Please give their GitHub username: '
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Chose the license for this project: ',
+            choices: [
+                "Apache",
+                "Boost",
+                "Eclipse",
+                "IBM",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Unlicense"
+            ]
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Is there a test included?'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
+    ])
 
-// TODO: Create a function to initialize app
-function init() {}
+}
 
-// Function call to initialize app
+// Async function using util.promisify 
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+
 init();
